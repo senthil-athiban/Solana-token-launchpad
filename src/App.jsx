c9c05c7fd651bcfac664a8e44f425a1ce1f9da28
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-07 20:51:27 +0530

    (feat): create token logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
+
+import { ToastContainer } from "react-toastify";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import "./App.css";
-import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  WalletModalProvider,
+} from "@solana/wallet-adapter-react-ui";
 import Appbar from "./components/Appbar";
-import '@solana/wallet-adapter-react-ui/styles.css';
 import TokenLaunchPad from "./components/TokenLaunchPad";
+import "react-toastify/dist/ReactToastify.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "./App.css";
+
 
 function App() {
   return (
-    <ConnectionProvider endpoint="https://api.devnet.solana.com">
-      <WalletProvider wallets={[]} autoConnect>
-        <WalletModalProvider>
-          <div className="bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 w-full">
-            <Appbar />
-            <div className="w-full h-screen flex justify-center items-center">
-              <TokenLaunchPad />
+    <div>
+      <ConnectionProvider endpoint="https://api.devnet.solana.com">
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div className="bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 w-full">
+              <Appbar />
+              <div className="w-full h-screen flex justify-center items-center">
+                <TokenLaunchPad />
+              </div>
             </div>
-          </div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+      <ToastContainer />
+    </div>
   );
 }
 
--- src/components/TokenLaunchPad.tsx
+++ src/components/TokenLaunchPad.tsx
@@ -1,23 +1,93 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  createInitializeMint2Instruction,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 
 const TokenLaunchPad = () => {
-    const [name, setName] = useState("");
-    const [symbol, setSymbol] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
-    const [initialSupply, setInitialSupply] = useState("");
+  const [name, setName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [initialSupply, setInitialSupply] = useState("");
 
-    const handleCreateToken = async () => {
-      
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const handleCreateToken = async () => {
+    if (!publicKey) {
+      toast.error("Wallet not connected");
+      return;
     }
-  return (
-    <div className='border px-2 py-4 flex flex-col gap-y-6 rounded-lg w-full max-w-md'>
-        <input type="text" className='bg-white bg-opacity-20 p-2' placeholder='Name' onChange={(e) => setName(e.target.value)}/>
-        <input type="text" className='bg-white bg-opacity-20 p-2' placeholder='Symbol' onChange={(e) => setSymbol(e.target.value)}/>
-        <input type="text" className='bg-white bg-opacity-20 p-2' placeholder='Image url' onChange={(e) => setImageUrl(e.target.value)}/>
-        <input type="text" className='bg-white bg-opacity-20 p-2' placeholder='Initial supply' onChange={(e) => setInitialSupply(e.target.value)}/>
-        <button className='bg-gradient-to-r from-pink-500 to-purple-500 rounded-lg hover:pink-300 p-2'>Create Token</button>
-    </div>
-  )
-}
+    const mintKeyPair = Keypair.generate();
 
-export default TokenLaunchPad
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: publicKey,
+        newAccountPubkey: mintKeyPair.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMint2Instruction(
+        mintKeyPair.publicKey,
+        6,
+        publicKey,
+        publicKey,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    transaction.feePayer = publicKey;
+    const recentBlockHash = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = recentBlockHash.blockhash;
+
+    // partially sign the transaction
+    transaction.partialSign(mintKeyPair);
+
+    // get confirmation from the payer in the launchpad and send the transaction onto sol blockchain
+    await sendTransaction(transaction, connection);
+  };
+  return (
+    <div className="border px-2 py-4 flex flex-col gap-y-6 rounded-lg w-full max-w-md">
+      <input
+        type="text"
+        className="bg-white bg-opacity-20 p-2"
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        className="bg-white bg-opacity-20 p-2"
+        placeholder="Symbol"
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+      <input
+        type="text"
+        className="bg-white bg-opacity-20 p-2"
+        placeholder="Image url"
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <input
+        type="text"
+        className="bg-white bg-opacity-20 p-2"
+        placeholder="Initial supply"
+        onChange={(e) => setInitialSupply(e.target.value)}
+      />
+      <button
+        className="bg-gradient-to-r from-pink-500 to-purple-500 rounded-lg hover:pink-300 p-2"
+        onClick={handleCreateToken}
+      >
+        Create Token
+      </button>
+    </div>
+  );
+};
+
+export default TokenLaunchPad;
