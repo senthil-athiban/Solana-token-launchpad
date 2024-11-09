bbb8c667cff7fd137f87df19d8fdbba3538bb09c
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-09 14:02:18 +0530

    (feat): attach meta data

--- src/components/TokenLaunchPad.tsx
+++ src/components/TokenLaunchPad.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import {
+  createInitializeMetadataPointerInstruction,
   createInitializeMint2Instruction,
+  createInitializeMintInstruction,
+  ExtensionType,
   getMinimumBalanceForRentExemptMint,
+  getMintLen,
+  LENGTH_SIZE,
   MINT_SIZE,
+  TOKEN_2022_PROGRAM_ID,
   TOKEN_PROGRAM_ID,
+  TYPE_SIZE,
 } from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import {
+  createInitializeInstruction,
+  pack,
+  TokenMetadata,
+} from "@solana/spl-token-metadata";
 
 const TokenLaunchPad = () => {
   const [name, setName] = useState("");
@@ -24,25 +36,41 @@ const TokenLaunchPad = () => {
       return;
     }
     const mintKeyPair = Keypair.generate();
+    const decimals = 9;
+    const metadata: TokenMetadata = {
+      mint: mintKeyPair.publicKey,
+      name: name,
+      symbol: symbol,
+      uri: imageUrl,
+      additionalMetadata: [["new-field", "new-value"]],
+    };
 
-    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+    const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+    const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+    
 
     const transaction = new Transaction().add(
       SystemProgram.createAccount({
-        fromPubkey: publicKey,
-        newAccountPubkey: mintKeyPair.publicKey,
-        space: MINT_SIZE,
-        lamports,
-        programId: TOKEN_PROGRAM_ID,
+          fromPubkey: publicKey,
+          newAccountPubkey: mintKeyPair.publicKey,
+          space: mintLen,
+          lamports,
+          programId: TOKEN_2022_PROGRAM_ID,
       }),
-      createInitializeMint2Instruction(
-        mintKeyPair.publicKey,
-        6,
-        publicKey,
-        publicKey,
-        TOKEN_PROGRAM_ID
-      )
-    );
+      createInitializeMetadataPointerInstruction(mintKeyPair.publicKey, publicKey, mintKeyPair.publicKey, TOKEN_2022_PROGRAM_ID),
+      createInitializeMintInstruction(mintKeyPair.publicKey, 9, publicKey, null, TOKEN_2022_PROGRAM_ID),
+      createInitializeInstruction({
+          programId: TOKEN_2022_PROGRAM_ID,
+          mint: mintKeyPair.publicKey,
+          metadata: mintKeyPair.publicKey,
+          name: metadata.name,
+          symbol: metadata.symbol,
+          uri: metadata.uri,
+          mintAuthority: publicKey,
+          updateAuthority: publicKey,
+      }),
+  );
 
     transaction.feePayer = publicKey;
     const recentBlockHash = await connection.getLatestBlockhash();
@@ -52,7 +80,8 @@ const TokenLaunchPad = () => {
     transaction.partialSign(mintKeyPair);
 
     // get confirmation from the payer in the launchpad and send the transaction onto sol blockchain
-    await sendTransaction(transaction, connection);
+    const res = await sendTransaction(transaction, connection);
+    console.log(' res : ', res);
   };
   return (
     <div className="border px-2 py-4 flex flex-col gap-y-6 rounded-lg w-full max-w-md">
